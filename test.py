19dbe372243147c82cf3d068ea2db4fe1402d2ef
heric moura <111160949+hericmr@users.noreply.github.com>
2023-02-07 21:39:24 -0300

    Add files via upload

--- test.py
+++ test.py
@@ -1,7 +1,16 @@
-from time import sleep
+#importa bibliotecas
 from selenium import webdriver
+from time import sleep
+from selenium.webdriver.common.by import By
+#inicializa o webdriver do chrome
+navegador = webdriver.Chrome()
 
-browser = webdriver.Chrome()
-browser.get("https://www.jornalsantista.com.br/")
+#acessa o site
+navegador.get("https://povosantista.blogspot.com/")
 
-sleep(15)
+#encontra o elemento da página com base no nome do elemento
+campo_de_busca = navegador.find_element(By.NAME, "q")
+#preenche campo de busca e envia
+campo_de_busca.send_keys('Héric Moura')
+campo_de_busca.submit()
+sleep(999)
